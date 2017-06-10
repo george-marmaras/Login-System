e71a4c229dc6f9161fe66433a07d4f2fde793cfb
george.marmaras <george-marmaras@users.noreply.github.com>
2017-06-10 15:42:46 +0300

    Login System
    A Simple Login System with MVC C# and custom encryption password functionality.

---
+++ SimpleLoginSystem/Controllers/UserController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SimpleLoginSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (IsValid(user.Email, user.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(user.Email, false);
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login credentials are incorrect!");
+                }
+            }
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(Models.UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                using(var db = new MyMainDbEntities())
+                {
+                    var crypto = new SimpleCrypto.PBKDF2();
+                    var encrypPass = crypto.Compute(user.Password);
+
+                    var sysUser = db.SystemUsers.Create();
+
+                    sysUser.Email = user.Email;
+                    sysUser.Password = encrypPass;
+                    sysUser.PasswordSalt = crypto.Salt;
+                    sysUser.UserId = Guid.NewGuid();
+
+                    db.SystemUsers.Add(sysUser);
+                    db.SaveChanges();
+
+                    FormsAuthentication.SetAuthCookie(user.Email, false);
+                    return RedirectToAction("Index", "User");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login credentials are incorrect.");
+            }
+
+            return View(user);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "User");
+        }
+
+        private bool IsValid(string email, string password)
+        {
+            var crypro = new SimpleCrypto.PBKDF2();
+
+            bool isValid = false;
+
+            using (var db = new MyMainDbEntities())
+            {
+                var user = db.SystemUsers.FirstOrDefault(u=>u.Email == email);
+
+                if (user != null)
+                {
+                    if (user.Password == crypro.Compute(password, user.PasswordSalt))
+                    {
+                        isValid = true;
+                    }
+                    
+                }
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ SimpleLoginSystem/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleLoginSystem.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        [Display(Name = "Email Address: ")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20,MinimumLength = 6)]
+        [Display(Name = "Password: ")]
+        public string Password { get; set; }
+        public string PasswordSalt { get; set; }
+    }
+}
